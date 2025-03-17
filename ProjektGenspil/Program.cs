bfff36c14f419d3a84308c53dac113f8b544ea50
Ninna Lundberg <nllu71404@edu.ucl.dk>
2025-03-17 10:19:25 +0100

    Oprettelse af Visual Studio projekt

---
+++ ProjektGenspil/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjektGenspil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej Team 5!");
+        }
+    }
+}
